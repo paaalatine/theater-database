0b88fd6f756fb79be970dfc43a47485556b8ead6
yashin-alexander <alexandr.yashin@emlid.com>
2017-11-24 05:24:03 +0300

    basic_operations: namedtuples added

--- python_implementation/basic_operations.py
+++ python_implementation/basic_operations.py
@@ -1,4 +1,19 @@
 import cx_Oracle
+from collections import namedtuple
+
+table = namedtuple('Name', 'name id fields')
+
+Service     = table("Service",      "serviceId",    ("serviceName",))
+Post        = table("Post",         "postId",       ("postName", "salary", "serviceId", "fk_post"))
+Staff       = table("Staff",        "StaffId",      ("name", "postId"))
+Performance = table("Performance",  "PerformanceId",("performanseName", "performansePrice", "description"))
+Role        = table("Role",         "RoleId",       ("roleName", "PerformanceId"))
+Cast        = table("Cast",         "CastId",       ("roleId", "personId"))
+Requisite   = table("Requisite",    "RequisiteId",  ("requisiteName", "requisitePrice"))
+Decoration  = table("Decoration",   "DecorationId", ("performanceId", "requisiteId"))
+Place       = table("Place",        "PlaceId",      ("placeType", "placePrice"))
+Timetable   = table("Timetable",    "TimetableId",  ("eventType", "eventDate", "performanceId"))
+Booking     = table("Booking",      "BookingId",    ("eventId", "placeId", "placeNo"))
 
 
 def fixup_fields(fields):
@@ -41,20 +56,20 @@ connection = cx_Oracle.connect('NAME/q@localhost:1521')
 cursor = connection.cursor()
 
 print('===================================================')
-create_template('REQUISITE', ('REQUISITENAME', 'REQUISITEPRICE'), ('brains', '1000'))
-select_template('REQUISITE')
+create_template(Requisite.name, Requisite.fields, ('brains', '1000'))
+select_template(Requisite.name)
 
 print('===================================================')
-delete_template('REQUISITE', 'REQUISITEID', 48)
-select_template('REQUISITE')
+delete_template(Requisite.name, Requisite.id, 48)
+select_template(Requisite.name)
 
 print('===================================================')
-create_template('SERVICE', ('SERVICENAME',), ('stage', ))
-select_template('SERVICE')
+create_template(Service.name, Service.fields, ('stage', ))
+select_template(Service.name)
 
 print('===================================================')
-delete_template('SERVICE', 'SERVICEID', 14)
-select_template('SERVICE')
+delete_template(Service.name, Service.id, 14)
+select_template(Service.name)
 
 # connection.commit()
 cursor.close()
