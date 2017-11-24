1062a26bbd582a4f9e4ebf0d1c6d797d6465c43c
yashin-alexander <alexandr.yashin@emlid.com>
2017-11-24 16:32:28 +0300

    basic_operations: update operation added
    namedtuples calls fixup

--- python_implementation/basic_operations.py
+++ python_implementation/basic_operations.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 table = namedtuple('Name', 'name id_field fields')
 
 Service     = table("service",      "serviceId",    ("serviceName",))
-Post        = table("post",         "postId",       ("postName", "salary", "serviceId", "fk_post"))
+Post        = table("post",         "postId",       ("postName", "salary", "serviceId"))
 Staff       = table("staff",        "StaffId",      ("name", "postId"))
 Performance = table("performance",  "PerformanceId",("performanseName", "performansePrice", "description"))
 Role        = table("role",         "RoleId",       ("roleName", "PerformanceId"))
@@ -53,44 +53,58 @@ def fixup_values(values):
     return values
 
 
-def create(table, fields, values):
-    fields = fixup_fields(fields)
+def create(table, values):
+    fields = fixup_fields(table.fields)
     values = fixup_values(values)
     try:
-        query = "INSERT INTO {} {} VALUES {}".format(table, fields, values)
+        query = "INSERT INTO {} {} VALUES {}".format(table.name, fields, values)
         cursor.execute(query)
         connection.commit()
     except cx_Oracle.DatabaseError:
-        info_string = "Specify '{}' correctly to commit create operation for '{}' table".format(fields, table)
+        info_string = "Specify '{}' correctly to commit create operation for '{}' table".format(fields, table.name)
         print(info_string)
 
 
 def read(table):
-    query = 'SELECT * FROM {}'.format(table)
+    query = 'SELECT * FROM {}'.format(table.name)
     cursor.execute(query)
     for result in cursor:
         print result
 
 
-def delete(table, field, value):
-    value = value[0]
-    query = 'DELETE FROM {} WHERE {}={}'.format(table, field, value)
+def update(table, values):
+    query = "UPDATE {} SET {}={} WHERE {}={}".format(table.name, values[1], values[2], table.id_field ,values[0])
     print(query)
     try:
         cursor.execute(query)
         connection.commit()
     except cx_Oracle.DatabaseError:
-        info_string = "Specify '{}' correctly to commit delete operation for '{}' table".format(field, table)
+        info_string = "Specify '{}', field to update: {}, value correctly to commit update operation for '{}' table".format(table.id_field, table.fields, table.name)
+        print(info_string)
+
+
+
+def delete(table, value):
+    value = value[0]
+    query = 'DELETE FROM {} WHERE {}={}'.format(table.name, table.id_field, value)
+    print(query)
+    try:
+        cursor.execute(query)
+        connection.commit()
+    except cx_Oracle.DatabaseError:
+        info_string = "Specify '{}' correctly to commit delete operation for '{}' table".format(table.id_field, table.name)
         print(info_string)
 
 
 def process_operation(table, operation, fields_info):
     if operation == "create":
-        create(table.name, table.fields, fields_info)
+        create(table, fields_info)
     if operation == "read":
-        read(table.name)
+        read(table)
+    if operation == "update":
+        update(table, fields_info)
     if operation == "delete":
-        delete(table.name, table.id_field, fields_info)
+        delete(table, fields_info)
 
 
 if __name__ == '__main__':
@@ -100,26 +114,5 @@ if __name__ == '__main__':
             exit(0)
     print("Specify table & operation correctly")
 
-    # print('===================================================')
-    # create(Requisite.name, Requisite.fields, ('brains', '1000'))
-    # read(Requisite.name)
-
-    # print('===================================================')
-    # create(Requisite.name, Requisite.fields, ('brains', '1000'))
-    # read(Requisite.name)
-    #
-    # print('===================================================')
-    # delete(Requisite.name, Requisite.id_field, 48)
-    # read(Requisite.name)
-    #
-    # print('===================================================')
-    # create(Service.name, Service.fields, ('stage',))
-    # read(Service.name)
-    #
-    # print('===================================================')
-    # delete(Service.name, Service.id_field, 14)
-    # read(Service.name)
-
-    # connection.commit()
     cursor.close()
     connection.close()
